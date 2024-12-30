c63526852df10c2387b7f3e600cd6be3ce388cdc
sehee-jj <158039404+sehee-jj@users.noreply.github.com>
2024-12-30 12:07:49 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 3.84 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    stack<int> ternary; // 3진법으로 변환한 수를 저장할 스택
+    
+    // 3진법 수로 변환
+    while(n>=3)
+    {
+        ternary.push(n%3);
+        n /= 3;
+    }
+    
+    ternary.push(n);
+    int size = ternary.size();
+    
+    // 10진법 수로 표현
+    for(int i=0; i<size; i++)
+    {
+        answer += (ternary.top()*pow(3,i));
+        ternary.pop();
+    }
+    return answer;
+}
