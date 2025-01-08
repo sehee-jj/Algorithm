2db05be4838b0cf880e49fb362a1d0482d5ebb40
sehee-jj <158039404+sehee-jj@users.noreply.github.com>
2025-01-08 17:05:54 +0900

    [level 1] Title: 삼총사, Time: 0.07 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/README.md
+++ 프로그래머스/1/131705. 삼총사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 삼총사 - 131705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.01 ms
+메모리: 4.19 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 08일 15:37:29
+2025년 01월 08일 17:05:52
 
 ### 문제 설명
 
--- 프로그래머스/1/131705. 삼총사/삼총사.cpp
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -2,26 +2,30 @@
 #include <vector>
 
 using namespace std;
-int solution(vector<int> number) {
-    int sum = 0;
-    int cnt = 0;
-    for(int i=0; i<number.size(); i++)
+
+int cnt = 0;
+
+void makeTrio(vector<int> number, vector<int> trio, int index)
+{
+    if(trio.size()>= 3)
     {
-        sum+=number.at(i);
-        for(int j=i+1; j<number.size(); j++)
-        {
-            sum+=number.at(j);
-            for(int k=j+1; k<number.size(); k++)
-            {
-                sum+=number.at(k);
-                if(sum==0)
-                    cnt++;
-                sum-=number.at(k);
-            }
-            sum-=number.at(j);
-        }
-        sum-=number.at(i);
+        int sum = trio.at(0) + trio.at(1) + trio.at(2);
+        cnt += (sum==0?1:0);
+        return;
     }
+    else if(index >= number.size())
+        return;
     
+    for(int i=index; i<number.size(); i++)
+    {
+        trio.push_back(number.at(i));
+        makeTrio(number, trio, i+1);
+        trio.pop_back();
+    }
+}
+
+int solution(vector<int> number) {
+    vector<int> trio;
+    makeTrio(number, trio, 0);
     return cnt;
 }
