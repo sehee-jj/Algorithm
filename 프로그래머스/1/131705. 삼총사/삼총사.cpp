c0bd2770806cb02555614712b4e438db0854f652
sehee-jj <158039404+sehee-jj@users.noreply.github.com>
2025-01-08 17:13:59 +0900

    [level 1] Title: 삼총사, Time: 0.07 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.cpp
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -3,29 +3,28 @@
 
 using namespace std;
 
-int cnt = 0;
-
-void makeTrio(vector<int> number, vector<int> trio, int index)
+int makeTrio(vector<int> number, vector<int> trio, int index)
 {
+    int result = 0;
     if(trio.size()>= 3)
     {
         int sum = trio.at(0) + trio.at(1) + trio.at(2);
-        cnt += (sum==0?1:0);
-        return;
+        return sum==0 ? 1:0;
     }
     else if(index >= number.size())
-        return;
+        return 0;
     
     for(int i=index; i<number.size(); i++)
     {
         trio.push_back(number.at(i));
-        makeTrio(number, trio, i+1);
+        result += makeTrio(number, trio, i+1);
         trio.pop_back();
     }
+    
+    return result;
 }
 
 int solution(vector<int> number) {
     vector<int> trio;
-    makeTrio(number, trio, 0);
-    return cnt;
+    return makeTrio(number, trio, 0);
 }
