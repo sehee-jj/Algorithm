80cff235cb20aaff5c64703a22a1b9d9352495ff
sehee-jj <158039404+sehee-jj@users.noreply.github.com>
2025-03-21 20:22:14 +0900

    [level 1] Title: 모의고사, Time: 0.12 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#) 
 
 ### 성능 요약
 
-메모리: 4.22 MB, 시간: 0.12 ms
+메모리: 4.21 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 21일 19:47:00
+2025년 03월 21일 20:22:12
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,112 +1,31 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <algorithm>
 
 using namespace std;
 
-
-vector<int> M2 = {1, 3, 4, 5};
-vector<int> M3 = {3, 1, 2, 4, 5};
-int M2_idx = 0;
-int M3_idx = 0;
-
-bool isCorrectM1(int idx,  int correct_answer)
-{
-    int answer = idx%5+1;
-    if(answer == correct_answer)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-bool isCorrectM2(int idx,  int correct_answer)
-{
-    bool result = false;
-    
-    if(idx%2 == 0)
-    {
-        if(correct_answer == 2)
-        {
-            result = true;
-        }
-    }
-    else
-    {
-        if(M2[M2_idx%4] == correct_answer)
-        {
-            result = true;
-        }
-        M2_idx++;
-    }
-    
-    return result;
-}
-
-bool isCorrectM3(int idx,  int correct_answer)
-{
-    bool result = false;
-    
-    if(correct_answer == M3[M3_idx%5])
-    {
-        result = true;
-    }
-    
-    if(idx%2 == 1)
-    {
-        M3_idx++;
-    }
-    
-    return result;
-}
-
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    map<int, int> result;
-    int M1_ID = 1;
-    int M2_ID = 2;
-    int M3_ID = 3;
     
-    for(int i=1; i<=3; i++)
-    {
-        result.insert({i, 0});
+    vector<int> scores = {0, 0, 0};
+    
+    vector<int> pattern1 = {1, 2, 3, 4, 5};
+    vector<int> pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
+    vector<int> pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
+    for(int i = 0; i < answers.size(); i++) {
+        if(answers[i] == pattern1[i % pattern1.size()]) scores[0]++;
+        if(answers[i] == pattern2[i % pattern2.size()]) scores[1]++;
+        if(answers[i] == pattern3[i % pattern3.size()]) scores[2]++;
     }
     
-    for(int i=0; i<answers.size(); i++)
-    {
-        if(isCorrectM1(i, answers[i]))
-        {
-            result[M1_ID]++;
-        }
-        
-        if(isCorrectM2(i, answers[i]))
-        {
-            result[M2_ID]++;
-        }
-        
-        if(isCorrectM3(i, answers[i]))
-        {
-            result[M3_ID]++;
-        }
-    }
+    int max_score = *max_element(scores.begin(), scores.end());
     
-    int max_score = 0;
-    for(auto res_math : result)
+    for(int i=0; i<scores.size(); i++)
     {
-        int man = res_math.first;
-        int score = res_math.second;
-        if(max_score < score)
+        if(scores[i]==max_score)
         {
-            max_score = score;
-            answer.clear();
-            answer.push_back(man);
-        }
-        else if(max_score == score)
-        {
-            answer.push_back(man);
+            answer.push_back(i+1);
         }
     }
     
